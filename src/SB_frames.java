f3f3a8a2c6dea071ee9b7a1e66c99c659229cbde
Shajeevan Panchardcharam <shajee96@hotmail.no>
2019-09-18 10:59:23 +0200

    TEST SHAJEEVAN

---
+++ src/SB_frames.java
@@ -0,0 +1,41 @@
+
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+
+public class SB_frames {
+    int width = 0;    //width of the image
+    int height = 0;   //height of the image
+    BufferedImage image = null;
+
+
+    public SB_frames(int width, int height, BufferedImage image) {
+        this.width = width;
+        this.height = height;
+        this.image = image;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+}
