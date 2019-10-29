e5a63692fb8f9e4f67c7f92d67cb6d8ccfd28cb1
Eilev <eilev.brustugun@outlook.com>
2019-10-29 10:24:46 +0100

    Added new CvCom and CVTuning

--- ServoCom.py
+++ ServoCom.py
@@ -1,8 +1,8 @@
 import socket
 import sys
-from adafruit_servokit import ServoKit
+#from adafruit_servokit import ServoKit
 
-kit = ServoKit(channels=16)
+#kit = ServoKit(channels=16)
 
 #UDP communication
 UDP_IP = '127.0.0.1'
@@ -14,12 +14,18 @@ s.bind((UDP_IP,UDP_PORT))
 
 while True:
     data, addr = s.recvfrom(1024)
+    data = data.decode()
     data = data.split('\n')[0].split(',')
 
     servo1 = data[0]
     servo2 = data[1]
     servo3 = data[2]
 
-    kit.servo[0].angle = servo1
-    kit.servo[1].angle = servo2
-    kit.servo[2].angle = servo3
+    print(servo1)
+    print(servo2)
+    print(servo3)
+    print("\n")
+
+    #kit.servo[0].angle = servo1
+    #kit.servo[1].angle = servo2
+    #kit.servo[2].angle = servo3
